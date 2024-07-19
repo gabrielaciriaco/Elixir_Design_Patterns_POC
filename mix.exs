e1c0ebc2d722642d44eedce9e4d2e781225c6786
gabrielaciriaco <gabs.ciriaco@gmail.com>
2024-07-19 18:05:55 -0300

    corrigindo mix.exs - para executar leia o readme

--- mix.exs
+++ mix.exs
@@ -25,22 +25,4 @@ defmodule ElixirDesignPatterns.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
-
-  def run do
-    # # factory
-    # IO.puts(ChannelFactory.f())
-    # IO.puts(ChannelFactory.g())
-    # IO.puts(ChannelFactory.h())
-
-    # # proxy
-    # BookSearchProxy.getBook("123456789")
-    # BookSearchProxy.getBook("987654321")
-
-    # # adapter
-    # projetor_samsung = %ProjetorSamsung{serial_number: "12345"}
-    # projetor_lg = %ProjetorLG{serial_number: "123456"}
-
-    # SistemaControleProjetores.init(AdaptadorProjetorSamsung)
-    # SistemaControleProjetores.init(AdaptadorProjetorLG)
-  end
 end
